0e51ffc2b875efcf2a1926bfbeb16f7755dd3540
originorion1 <321457759+originorion1@users.noreply.github.com>
2026-08-28 20:37:28 +0300

    feat(understanding): expose system graph domain contracts

---
+++ src/orion/understanding/__init__.py
@@ -0,0 +1,25 @@
+"""System-understanding domain components."""
+
+from .graph import (
+    GraphInvariantError,
+    GraphNode,
+    GraphPath,
+    GraphQuery,
+    GraphRelationship,
+    GraphStatus,
+    GraphStore,
+    NodeType,
+    RelationshipType,
+)
+
+__all__ = [
+    "GraphInvariantError",
+    "GraphNode",
+    "GraphPath",
+    "GraphQuery",
+    "GraphRelationship",
+    "GraphStatus",
+    "GraphStore",
+    "NodeType",
+    "RelationshipType",
+]
